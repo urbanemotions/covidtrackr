7b049cea15e7c10c6ac3f12eed0686fc41005467
sin wan wildy <sw.wildy@gmail.com>
2021-02-18 23:15:35 -0800

    thursday1115

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,23 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def edit
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
             redirect_to @user
         else
             render :new
+        end
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit 
         end
     end
 
